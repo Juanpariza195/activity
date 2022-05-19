a83b88119064fbd207b2dd0636d98dfb21baacad
juan ariza <juanpariza195@gmail.com>
2022-05-18 23:06:08 -0500

    primer commit

---
+++ parteone.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 22:53:04 2022
+
+@author: DELL3467
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import neighbors
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from warnings import simplefilter
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
+simplefilter(action='ignore', category=FutureWarning)
+
+url = 'bank-full.csv'
+data = pd.read_csv(url)
+
+# Tratamiento de la data
+data.marital.replace(['married', 'divorced', 'single'],
+                     [0, 1, 2], inplace=True)
+data.education.replace(['primary', 'secondary', 'tertiary', 'unknown'], [
+                       0, 1, 2, 3], inplace=True)
+data.default.replace(['no', 'yes'], [0, 1], inplace=True)
+data.housing.replace(['no', 'yes'], [0, 1], inplace=True)
+data.loan.replace(['no', 'yes'], [0, 1], inplace=True)
+data.poutcome.replace(['failure', 'other', 'success', 'unknown'], [
+                      0, 1, 2, 3], inplace=True)
+
+data.drop(['job', 'balance', 'contact', 'day', 'month', 'duration',
+          'pdays', 'previous', 'campaign'], axis=1, inplace=True)
+
+data.age.replace(np.nan, 41, inplace=True)
+rangos = [0, 8, 15, 18, 25, 40, 60, 100]
+nombres = ['1', '2', '3', '4', '5', '6', '7']
+data.age = pd.cut(data.age, rangos, labels=nombres)
+
+data.dropna(axis=0, how='any', inplace=True)
+data.y.replace(['no', 'yes'], [0, 1], inplace=True)
+
+# partir la data en dos
+
+data_train = data[:728]
+data_test = data[728:]
+
+x = np.array(data_train.drop(['y'], 1))
+y = np.array(data_train.y)  # 0 NO 1 Si
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_test_out = np.array(data_test.drop(['y'], 1))
+y_test_out = np.array(data_test.y)  # 0 No 1 Si
+
+# Regresión Logística
+
+# Seleccionar un modelo
+logreg = LogisticRegression(solver='lbfgs', max_iter=7600)
+
+# REGRESIÓN LOGÍSTICA CON VALIDACIÓN CRUZADA
+
+kfold = KFold(n_splits=10)
+
+acc_scores_train_train = []
+acc_scores_test_train = []
+logreg = LogisticRegression(solver='lbfgs', max_iter=7600)
+
+for train, test in kfold.split(x, y):
+    logreg.fit(x[train], y[train])
+    scores_train_train = logreg.score(x[train], y[train])
+    scores_test_train = logreg.score(x[test], y[test])
+    acc_scores_train_train.append(scores_train_train)
+    acc_scores_test_train.append(scores_test_train)
+
+y_pred = logreg.predict(x_test_out)
+
+print('*' * 50)
+print('Regresión Logística Validación cruzada')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(
+    f'accuracy de Entrenamiento de Entrenamiento: {np.array(acc_scores_train_train).mean()}')
+
+# Accuracy de Test de Entrenamiento
+print(
+    f'accuracy de Test de Entrenamiento: {np.array(acc_scores_test_train).mean()}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {logreg.score(x_test_out, y_test_out)}')
+
+# Matriz de confusión
+print(f'Matriz de confusión: {confusion_matrix(y_test_out, y_pred)}')
+
+matriz_confusion = confusion_matrix(y_test_out, y_pred)
+plt.figure(figsize=(6, 6))
+sns.heatmap(matriz_confusion)
+plt.title("Mariz de confución")
+
+precision = precision_score(y_test_out, y_pred, average=None).mean()
+print(f'Precisión: {precision}')
+
+recall = recall_score(y_test_out, y_pred, average=None).mean()
+print(f'Re-call: {recall}')
+
+f1_score_logred = f1_score(y_test_out, y_pred, average=None).mean()
+
+print(f'f1: {f1_score_logred}')
+
